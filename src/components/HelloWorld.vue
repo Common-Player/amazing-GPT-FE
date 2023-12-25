89952511f9c8501c7f8dd2e4dfae401b69feb8b3
yaoerzhuang <yaoerzhuang@baidu.com>
2023-12-25 16:17:11 +0800

    美化样式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="hello">
-    <!-- 添加一个输入框用于输入用户请求 -->
-    <input type="text" v-model="userInput" placeholder="输入您的请求" />
+    <el-input
+      v-model="userInput"
+      placeholder="输入您的请求"
+      class="input-field"
+    ></el-input>
 
-    <!-- 添加一个按钮来触发请求 -->
-    <button @click="sendBaiduRequest">发送文心一言请求</button>
-    <button @click="sendGPTRequest">发送GPT请求</button>
+    <el-select
+      v-model="selectedApis"
+      multiple
+      placeholder="请选择API"
+      class="select-field"
+    >
+      <el-option label="百度" value="baidu"></el-option>
+      <el-option label="GPT" value="gpt"></el-option>
+    </el-select>
 
-    <!-- 可以在这里显示响应结果 -->
-    <div v-if="response">
+    <el-button type="primary" @click="sendRequests" class="send-button"
+      >发送请求</el-button
+    >
+
+    <div v-if="response" class="response">
       <h3>百度响应结果:</h3>
       <pre>{{ response.result }}</pre>
     </div>
-    <div v-if="response2">
+    <div v-if="response2" class="response">
       <h3>GPT响应结果:</h3>
       <pre>{{ response2.choices[0].message.content }}</pre>
     </div>
@@ -30,11 +42,29 @@ export default {
   data() {
     return {
       userInput: "", // 用户输入的数据
-      response: null, // 用于存储响应数据
-      response2: null, // 用于存储响应数据
+      selectedApis: ["baidu", "gpt"], // 选择的API类型数组
+      response: null, // 存储百度的响应数据
+      response2: null, // 存储GPT的响应数据
     };
   },
   methods: {
+    async sendRequests() {
+      const requests = [];
+
+      if (this.selectedApis.includes("baidu")) {
+        requests.push(this.sendBaiduRequest());
+      }
+      if (this.selectedApis.includes("gpt")) {
+        requests.push(this.sendGPTRequest());
+      }
+
+      try {
+        await Promise.all(requests);
+      } catch (error) {
+        console.error("请求 API 出错:", error);
+      }
+    },
+
     async sendGPTRequest() {
       const OPENAI_API_KEY = process.env.VUE_APP_OPENAI_API_KEY;
       try {
@@ -90,26 +120,43 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+
+.input-field,
+.select-field,
+.send-button {
+  width: 300px; /* 调整宽度 */
+}
+
+.response {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+  border: 1px solid #eee;
+  padding: 10px;
+  width: 60%;
+  max-height: 300px; /* 设置最大高度 */
+}
+pre {
+  width: 80%; /* 设置固定宽度 */
+  max-height: 150px; /* 设置最大高度 */
+  overflow-y: auto; /* 超出部分垂直滚动 */
+  background-color: #f9f9f9; /* 背景色，可根据需要调整 */
+  padding: 10px; /* 内边距 */
+  border: 1px solid #ccc; /* 边框 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* 阴影效果，可选 */
+  white-space: pre-wrap; /* 自动换行 */
+  word-wrap: break-word; /* 长单词或URL可以换行分割 */
+}
+
 h3 {
-  margin: 40px 0 0;
-}
-input[type="text"] {
-  margin: 20px 0;
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-button {
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  background-color: #42b983;
-  color: white;
-  cursor: pointer;
-}
-button:hover {
-  background-color: #333;
+  color: #333;
 }
 </style>
